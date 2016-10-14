022a55a04e9b30038b6111efa38cd26119aeae36
Andy Aylward <aaylward@gmail.com>
2016-10-14 00:29:23 -0400

    reverse

--- recurse.erl
+++ recurse.erl
@@ -1,5 +1,5 @@
 -module(recurse).
--export([length/1, duplicate/2]).
+-export([length/1, duplicate/2, reverse/1]).
 
 length(List) -> length(List, 0).
 
@@ -10,3 +10,8 @@ duplicate(Item, Number) -> duplicate(Item, Number, []).
 
 duplicate(_, 0, Acc) -> Acc;
 duplicate(Item, Number, Acc) when Number > 0 -> duplicate(Item, Number - 1, [Item | Acc]).
+
+reverse(List) -> reverse(List, []).
+
+reverse([], Acc) -> Acc;
+reverse([Head|Tail], Acc) -> reverse(Tail, [Head|Acc]).
