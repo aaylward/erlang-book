e5673af4f1fcef951faa9831d9a1657ea4bb334f
Andy Aylward <aaylward@gmail.com>
2016-10-14 16:44:02 -0400

    add firstN

--- recurse.erl
+++ recurse.erl
@@ -1,17 +1,24 @@
 -module(recurse).
--export([length/1, duplicate/2, reverse/1]).
+-export([length/1, duplicate/2, reverse/1, firstN/2]).
 
 length(List) -> length(List, 0).
 
 length([], Acc) -> Acc;
 length([_|T], Acc) -> length(T, Acc + 1).
 
-duplicate(Item, Number) -> duplicate(Item, Number, []).
+duplicate(Item, N) -> duplicate(Item, N, []).
 
 duplicate(_, 0, Acc) -> Acc;
-duplicate(Item, Number, Acc) when Number > 0 -> duplicate(Item, Number - 1, [Item | Acc]).
+duplicate(Item, N, Acc) -> duplicate(Item, N - 1, [Item|Acc]).
 
 reverse(List) -> reverse(List, []).
 
 reverse([], Acc) -> Acc;
-reverse([Head|Tail], Acc) -> reverse(Tail, [Head|Acc]).
+reverse([H|T], Acc) -> reverse(T, [H|Acc]).
+
+firstN(List, N) -> reverse(firstN(List, N, [])).
+
+firstN([], _, Acc) -> Acc;
+firstN(_, 0, Acc) -> Acc;
+firstN([H|T], N, Acc) -> firstN(T, N - 1, [H|Acc]).
+
